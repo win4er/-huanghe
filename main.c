3c6e21bb702ce0b18393e0d9e6b4e0a8cd80e87b
win4er <platon160106@gmail.com>
2023-02-02 21:08:44 +0300

    fixed main.c

--- main.c
+++ main.c
@@ -7,21 +7,19 @@ int foo() {
 	return countCall;
 }
 
-int foo1() {
-	static int countCall = 0;
-}
 
 int show() {
 
 	static int deepRec = 0;
 	deepRec++;
-
+    foo();
 	std::cout << deepRec << std::endl;
 	if (deepRec > 5) {
 		std::cout << "0 end show" << std::endl;
 		return deepRec;
+		deepRec = 0;
 	}
-	deepRec = 0;
+
 	show();
 
 	std::cout << "end show" << std::endl;
@@ -33,10 +31,8 @@ int main() {
 
 	show();
 	show();
-	return 0;
 
-	for (int i = 0; i < 10; i++) 
-		std::cout << 	foo() << std::endl;
+	std::cout << foo() << std::endl;
 
 	return 0;
 }
